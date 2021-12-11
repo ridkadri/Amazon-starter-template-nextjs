fd2847860424f496504e2590932556ce2f9b88f3
Ridwan Kadri <kadriridwan0@gmail.com>
2021-12-11 23:38:53 +0000

    day 3 code

---
+++ firebase.js
@@ -0,0 +1,10 @@
+import firebase from 'firebase';
+
+const firebaseConfig = {
+    apiKey: "AIzaSyBA5tJxrU94vDlK26lyVrZzjp_c_7ZVoRQ",
+    authDomain: "nextjs-c4b64.firebaseapp.com",
+    projectId: "nextjs-c4b64",
+    storageBucket: "nextjs-c4b64.appspot.com",
+    messagingSenderId: "912649643661",
+    appId: "1:912649643661:web:5f8cac03881622317824c4"
+};
